e7bf677fe101349849ea76e55b9e66f872ad37f3
Prittam GOSWAMI <prittam32@gmail.com>
2024-12-10 17:52:50 +0530

    minor changes-Dec102024

--- msa_tree_construction.py
+++ msa_tree_construction.py
@@ -4,17 +4,17 @@ import subprocess
 
 
 
-
 def run_mafft(input_fasta, output_fasta, threads=3):
     """
-    Runs MAFFT alignment on a given FASTA file using a specified number of threads.
+    Runs MAFFT alignment on a given FASTA file  
+    with a specified number of threads.
 
     Parameters:
-    input_fasta (str): Path to the input FASTA file.
-    output_fasta (str): Path where the aligned output will be saved.
-    threads (int): Number of threads to use (default is 3).
+        - input_fasta (str): Path to the input FASTA file.
+        - output_fasta (str): Path where the aligned output will be saved.
+        - threads (int): Number of threads to use (default is 3).
     """
-    # MAFFT command with the specified number of threads
+    # MAFFT command with specified number of threads
     command = ["mafft", "--thread", str(threads), input_fasta]
 
     try:
@@ -28,32 +28,27 @@ def run_mafft(input_fasta, output_fasta, threads=3):
 
 
 
-
-
-def create_phylogenetic_tree(alignment_file, outgroup_sequence, sample, threads=3):
+def create_phylogenetic_tree(alignment_file, outgroup_sequence, sample, threads,):
     """
     Create a rooted IQ-TREE phylogenetic tree by specifying the first sequence in the alignment as the outgroup.
 
     Parameters:
-        alignment_file (str): Path to the input alignment file.
-        outgroup_sequence (str): Identifier of the outgroup sequence in the alignment.
-        sample (str) : Denotes the sample number 
-        threads (int): Number of threads to use (default is 3).
-
-    Returns:
-        str: Path to the output tree file containing the rooted phylogenetic tree.
+        - alignment_file (str): Path to the input alignment file.
+        - outgroup_sequence (str): Identifier of the outgroup sequence in the alignment.
+        - sample (str): Depicts the Sample number to which the alignment file belongs.
+        - threads (int): Number of threads to use for running the IQTREE.
     """
     # Define the IQ-TREE command with the outgroup specified
     iqtree_cmd = [
         "iqtree2",
         "-s", alignment_file,
-        "-o", outgroup_sequence,  # Specify the outgroup sequence
-        "-m", "GTR+F+I+G4",  # Substitution model 
-        "-nt", str(threads),  # Number of threads 
-        "-redo",  # Redo the analysis from scratch if the results already exists
-        "-fast",  # use fasttree contrusction algorithm
-        "-asr",  # ancestral state reconstruction
-        "-pre", f"../Samples/{sample}/{sample}"
+        "-o", outgroup_sequence,  # the outgroup sequence
+        "-m", "GTR+F+I+G4",  # Substitution model
+        "-nt", str(threads),  # Number of threads to use
+        "-redo",  # Redo the analysis from scratch if it already exists
+        "-fast",  # Use the fasttree algorithm for tree construction
+        "-asr",  # Ancestral state reconstruction
+        "-pre", f"../Samples/{sample}/{sample}" # Location to save the generated files
     ]
 
     # Execute the IQ-TREE command
@@ -61,14 +56,26 @@ def create_phylogenetic_tree(alignment_file, outgroup_sequence, sample, threads=
         subprocess.run(iqtree_cmd, check=True)
         print("IQ-TREE analysis completed successfully.")
     except subprocess.CalledProcessError as e:
-        print(f"Error running IQ-TREE: {sample}", e)
+        print("Error running IQ-TREE:", e)
+
 
 
 def construct_msa_and_phylogenetic_tree(folder_path, outgroup_sequence, sample_size, threads_to_be_used):
+    """
+    Construct the Multiple Sequence Alignment and the Phylogenetic tree for all the sampled genome sets
+
+    Parameters:
+        - folder_path (str): The path to the folder containing all the sampled genomesets sub-directories
+        - outgroup_sequence (str): Identifier of the outgroup sequence.
+        - sample_size (str): Number of Sampled genomes in each Genome set
+        - threads_to_be_used (int): Number of threads to use for running the IQTREE.
+    """
+    
+    
     # Iterate over all samples in the directory
     for sample in os.listdir(folder_path):
         print(sample)
-        # Create the full path for each item
+        # Designate the full path for each multifasta file corresponding to each sampled genomeset
         input_fasta = os.path.join(folder_path, sample, f"SARS-CoV-2_{sample}_{sample_size}+1.fasta")
         # construct MSA output file path
         msa_output_fasta = os.path.join(folder_path, sample, f"SARS-CoV-2_{sample}_{sample_size}+1_msa.fasta")
@@ -77,7 +84,7 @@ def construct_msa_and_phylogenetic_tree(folder_path, outgroup_sequence, sample_s
         create_phylogenetic_tree(msa_output_fasta, outgroup_sequence, sample, threads_to_be_used)
 
 # Run the Code
-sample_size=3600
+sample_size=4000
 outgroup_sequence = "NC_045512.2"
 threads_to_be_used=30        
-construct_msa_and_phylogenetic_tree("../Samples", outgroup_sequence,sample_size, threads_to_be_used)
+construct_msa_and_phylogenetic_tree("../Samples/", outgroup_sequence,sample_size, threads_to_be_used)
